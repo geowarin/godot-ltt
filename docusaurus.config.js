cc38958603544b3d2eff6f21432128fed5d1eb57
Geoffroy Warin <code@geowarin.com>
2021-11-05 12:48:11 +0100

    fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Godot LTT',
   tagline: 'LTT',
   url: 'https://geowarin.com',
-  baseUrl: '/godot-ltt',
+  baseUrl: '/godot-ltt/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
